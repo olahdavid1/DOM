43ece020bc08e79a3aacfc20e25ee1e4f7c4c01b
Your Name <you@example.com>
2023-02-07 15:54:47 +0100

    uzenet

---
+++ script.js
@@ -0,0 +1,15 @@
+/*console.dir(document);
+console.log(document.URL);
+console.log(document.title);
+document.title = "document"
+console.log(document.head)*/
+
+
+let items = document.getElementsByClassName("item");
+console.log(items);
+
+for (let i = 0; i < items.length; i++) {
+    if (i % 2 == 1) {
+        items[i].s
+    }
+}
